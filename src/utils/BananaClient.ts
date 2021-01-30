14d16f74c203a3578c1d603d8d7143d3dba7afa0
pikokr <parangee9706@gmail.com>
2021-01-30 17:10:02 +0900

    feat: init erela.js

--- src/utils/BananaClient.ts
+++ src/utils/BananaClient.ts
@@ -1,9 +1,25 @@
-import { Client, Message, Team, User } from 'discord.js'
+import {
+  Client,
+  Collection,
+  Guild,
+  Message,
+  MessageEmbed,
+  Team,
+  User,
+} from 'discord.js'
 import Dokdo from 'dokdo'
 import path from 'path'
 import fs from 'fs'
 import { Command } from './typings'
 import chokidar from 'chokidar'
+import { Manager } from 'erela.js'
+
+declare module 'discord.js' {
+  interface Client {
+    music: Manager
+    controllerMap: Collection<string, Message>
+  }
+}
 
 export default class BananaClient extends Client {
   owners: string[] = []
@@ -29,7 +45,139 @@ export default class BananaClient extends Client {
         }, 1000)
       }
     })
+    this.controllerMap = new Collection()
     this.on('message', this.executeCommand.bind(this))
+    this.music = new Manager({
+      send: (id, payload) => {
+        const guild = this.guilds.cache.get(id)
+        if (guild) guild.shard.send(payload)
+      },
+      nodes: this.config.nodes,
+    })
+
+    this.music.on('nodeConnect', (node) => {
+      console.log(`Node ${node.options.host}:${node.options.port} connected.`)
+    })
+    this.music.on('nodeError', (node, error) => {
+      console.log(
+        `Node ${node.options.host}:${node.options.port} encounted an error: ${error.message}`,
+      )
+    })
+    this.music.on('queueEnd', (player) => {
+      player.destroy()
+    })
+
+    this.music.on('nodeRaw', async (payload: any) => {
+      if (payload.op === 'playerUpdate') {
+        const guild = this.guilds.cache.get(payload.guildId)
+        if (guild) {
+          let m = this.controllerMap.get(guild.id)
+          if (m?.deleted) {
+            this.controllerMap.set(
+              guild.id,
+              await m.channel
+                .send(BananaClient.getNowPlayingEmbed(guild))
+                .then((r) => {
+                  BananaClient.initController(r)
+                  return r
+                }),
+            )
+            return
+          }
+          if (m) {
+            m.edit(BananaClient.getNowPlayingEmbed(guild)).catch(async () => {
+              const msg = await m?.channel.send(
+                BananaClient.getNowPlayingEmbed(guild),
+              )!
+              this.controllerMap.set(guild.id, msg)
+              await BananaClient.initController(msg)
+              return
+            })
+          }
+        }
+      }
+    })
+
+    this.on('ready', () => this.music.init(this.user!.id))
+    this.on('raw', (payload) => this.music.updateVoiceState(payload))
+  }
+
+  static async initController(msg: Message) {
+    if ((msg as any).controllerInitialized) return
+    const emojis = ['⏯️', '⏹️', '▶️', '🔄', '➕', '➖']
+
+    ;(msg as any).controllerInitialized = true
+
+    await Promise.all(emojis.map((r) => msg.react(r)))
+  }
+
+  static formatTime(duration: number) {
+    const d = new Date(0)
+    d.setMilliseconds(duration)
+    return d.toISOString().substr(11, 8)
+  }
+
+  static createBar(
+    total: number,
+    current: number,
+    size = 15,
+    line = '▬',
+    slider = '🔘',
+  ) {
+    if (current > total) {
+      const bar = line.repeat(size + 2)
+      const percentage = (current / total) * 100
+      return [bar, percentage]
+    } else {
+      const percentage = current / total
+      const progress = Math.round(size * percentage)
+      const emptyProgress = size - progress
+      const progressText = line.repeat(progress).replace(/.$/, slider)
+      const emptyProgressText = line.repeat(emptyProgress)
+      const bar = progressText + emptyProgressText
+      const calculated = percentage * 100
+      return [bar, calculated]
+    }
+  }
+
+  static getNowPlayingEmbed(guild: Guild): MessageEmbed {
+    const embed = new MessageEmbed()
+    const player = guild.client.music.players.get(guild.id)
+    if (!player || !player.queue.current)
+      embed.setTitle('재생중인 곡이 없네요!')
+    else {
+      const t = player.queue.current
+      embed.setTitle(
+        `${player.playing ? ':arrow_forward:' : ':pause_button:'} ${t.title}`,
+      )
+      embed.setThumbnail(t.displayThumbnail?.('maxresdefault')!)
+      embed.setDescription(
+        `${this.formatTime(player.position)} ${
+          this.createBar(t.duration!, player.position)[0]
+        } -${this.formatTime(t.duration! - player.position)}`,
+      )
+      embed.addFields([
+        {
+          name: '볼륨',
+          value: player.volume + '%',
+          inline: true,
+        },
+        {
+          name: '반복 모드',
+          value: player.queueRepeat
+            ? '대기열 전체 반복'
+            : player.trackRepeat
+            ? '현재 곡 반복'
+            : '반복 안함',
+          inline: true,
+        },
+      ])
+      embed.setFooter(
+        (t.requester as any).tag,
+        (t.requester as any).displayAvatarURL({ dynamic: true }),
+      )
+    }
+    return embed
   }
 
   async executeCommand(msg: Message) {
